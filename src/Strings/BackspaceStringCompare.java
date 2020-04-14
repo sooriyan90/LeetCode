33d6b4096c3ca460969897f37894dd4bb32bd569
narayanan.sekhar <narayanan.sekhar@192.168.0.173>
2020-04-13 18:25:27 -0600

    changed package name to Strings

--- src/Strings/BackspaceStringCompare.java
+++ src/Strings/BackspaceStringCompare.java
@@ -1,4 +1,4 @@
-package StringManipulation;
+package Strings;
 
 /**
  Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
