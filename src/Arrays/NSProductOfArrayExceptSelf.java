dd55b0f2d28ffe0d722ad0c000f7051c81d0f7c6
narayanan.sekhar <narayanan.sekhar@c02rrafmfvh5>
2020-04-16 00:40:44 -0600

    question and solution for ProductOfArrayExceptSelf

---
+++ src/Arrays/NSProductOfArrayExceptSelf.java
@@ -0,0 +1,32 @@
+package Arrays;
+/*Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Example:
+
+Input:  [1,2,3,4]
+Output: [24,12,8,6]
+Constraint: It's guaranteed that the product of the elements of any prefix or suffix of the array (including the whole array) fits in a 32 bit integer.
+
+Note: Please solve it without division and in O(n).
+
+Follow up:
+Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)*/
+
+class NSProductOfArrayExceptSelf {
+    public int[] productExceptSelf(int[] nums) {
+        int[] op = new int[nums.length];
+        int temp = 1;
+        for(int i = nums.length - 1; i > 0; i--){
+            op[i] = temp*nums[i];
+            temp = op[i];
+        }
+        op[0] = op[1];
+        temp = 1;
+        for(int i = 0; i < nums.length - 2; i++){
+            temp = temp*nums[i];
+            op[i+1] = temp*op[i+2];
+        }
+        op[nums.length - 1] = temp*nums[nums.length - 2];
+        return op;
+    }
+}
